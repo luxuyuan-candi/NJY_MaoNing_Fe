b71381b91c223a7e637346514001dda6ab77fc47
luxuyuan-candi <luweike77@gmail.com>
2026-04-24 01:02:12 +0800

    adjust position

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,10 @@ const { asset } = require('../../utils/request');
 Page({
   data: {
     videoList: [
-      { id: 0, title: '宣传视频一', src: asset('/public-assets/cat-video/video_1.mp4') },
-      { id: 1, title: '宣传视频二', src: asset('/public-assets/cat-video/video_2.mp4') },
-      { id: 2, title: '宣传视频三', src: asset('/public-assets/cat-video/video_3.mp4') },
-      { id: 3, title: '宣传视频四', src: asset('/public-assets/cat-video/video_4.mp4') },
+      { id: 0, title: '一', src: asset('/public-assets/cat-video/video_1.mp4') },
+      { id: 1, title: '二', src: asset('/public-assets/cat-video/video_2.mp4') },
+      { id: 2, title: '三', src: asset('/public-assets/cat-video/video_3.mp4') },
+      { id: 3, title: '四', src: asset('/public-assets/cat-video/video_4.mp4') },
     ],
     currentVideoIndex: 0,
   },
