86e67601d2d051c56ac3b795deb444c608ef12e5
root <root@localhost.localdomain>
2026-04-23 21:30:06 +0800

    Switch miniapp to trusted ingress domain

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiBaseUrl: 'https://10.0.0.14:30443',
-  assetBaseUrl: 'https://10.0.0.14:30443',
+  apiBaseUrl: 'https://www.njwjxy.cn:30443',
+  assetBaseUrl: 'https://www.njwjxy.cn:30443',
 };
