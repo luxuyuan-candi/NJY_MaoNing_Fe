bc6708c3b2eb8e858423735bba1b3f1c251f88e3
root <root@localhost.localdomain>
2026-04-23 23:40:46 +0800

    fix: simplify homepage video carousel

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,10 @@ Page({
   data: {
     swiperInterval: 5000,
     videoList: [
-      { src: asset('/cat-video/video_1.mp4') },
-      { src: asset('/cat-video/video_2.mp4') },
-      { src: asset('/cat-video/video_3.mp4') },
-      { src: asset('/cat-video/video_4.mp4') },
+      { src: asset('/public-assets/cat-video/video_1.mp4') },
+      { src: asset('/public-assets/cat-video/video_2.mp4') },
+      { src: asset('/public-assets/cat-video/video_3.mp4') },
+      { src: asset('/public-assets/cat-video/video_4.mp4') },
     ],
   },
 
@@ -17,17 +17,5 @@ Page({
 
   onVideoEnded() {
     this.setData({ swiperInterval: 5000 });
-  },
-
-  goToYaoZha() {
-    wx.switchTab({ url: '/pages/yaozha/index' });
-  },
-
-  goToMaoSha() {
-    wx.switchTab({ url: '/pages/cat/index' });
-  },
-
-  goToProfile() {
-    wx.switchTab({ url: '/pages/profile/index' });
   },
 });
