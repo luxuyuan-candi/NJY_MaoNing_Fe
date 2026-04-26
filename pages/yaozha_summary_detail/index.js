bb2e4812b75c34d318f81fc71d80552450406be9
root <root@localhost.localdomain>
2026-04-26 19:42:23 +0800

    feat: redesign recycle summary detail

--- pages/yaozha_summary_detail/index.js
+++ pages/yaozha_summary_detail/index.js
@@ -36,6 +36,7 @@ Page({
       const lineData = [...barData];
 
       this.setData({
+        unit: res.data.data.name || unit,
         location: res.data.data.location,
         total,
         categories,
@@ -63,25 +64,98 @@ Page({
 
       const { categories, barData, lineData } = this.data;
       chart.setOption({
-        tooltip: { trigger: 'axis' },
-        legend: { data: ['柱状图', '折线图'] },
+        color: ['#5b86e5', '#63b357'],
+        tooltip: {
+          trigger: 'axis',
+          backgroundColor: 'rgba(255,255,255,0.96)',
+          borderColor: '#dbe8d8',
+          textStyle: { color: '#304338' },
+        },
+        legend: {
+          top: 8,
+          right: 0,
+          itemWidth: 26,
+          itemHeight: 16,
+          textStyle: {
+            color: '#33413a',
+            fontSize: 13,
+          },
+          data: ['柱状图', '折线图'],
+        },
+        grid: {
+          left: 44,
+          right: 18,
+          top: 72,
+          bottom: 42,
+        },
         xAxis: {
           type: 'category',
           data: categories,
+          axisLine: {
+            lineStyle: { color: '#7cb08a' },
+          },
+          axisTick: { show: false },
+          axisLabel: {
+            color: '#47584e',
+            fontSize: 11,
+          },
         },
         yAxis: {
           type: 'value',
+          name: '回收量（kg）',
+          nameTextStyle: {
+            color: '#50635a',
+            fontSize: 12,
+            padding: [0, 0, 8, -28],
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#d8e4d8',
+              type: 'dashed',
+            },
+          },
+          axisLine: { show: false },
+          axisTick: { show: false },
+          axisLabel: {
+            color: '#64766d',
+            fontSize: 11,
+          },
         },
         series: [
           {
             name: '柱状图',
             type: 'bar',
             data: barData,
+            barWidth: '42%',
+            itemStyle: {
+              borderRadius: [8, 8, 0, 0],
+              shadowColor: 'rgba(83, 120, 212, 0.22)',
+              shadowBlur: 18,
+            },
+            label: {
+              show: true,
+              position: 'top',
+              formatter: ({ value }) => `${value} kg`,
+              color: '#ffffff',
+              backgroundColor: '#4da44a',
+              borderRadius: 10,
+              padding: [6, 10],
+            },
           },
           {
             name: '折线图',
             type: 'line',
             data: lineData,
+            smooth: false,
+            symbol: 'circle',
+            symbolSize: 10,
+            lineStyle: {
+              width: 3,
+            },
+            itemStyle: {
+              borderWidth: 2,
+              borderColor: '#ffffff',
+            },
           },
         ],
       });
